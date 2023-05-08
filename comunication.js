7e2cc57d38aabc47919a2e2f7e906201e07ab49d
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2023-05-08 19:36:05 -0300

    Create comunication.js

---
+++ comunication.js
@@ -0,0 +1,32 @@
+
+class Comunication {    
+    static start() {
+        Comunication.arrayOfMessages = []
+    }
+
+    static insertNewMessage(valueOfMessage,toAgent) {
+        Comunication.arrayOfMessages.push(
+            {
+                valueOfMessage,
+                toAgent
+            }
+        )
+    }
+
+    static getArrayOfMessage() {
+        return Comunication.arrayOfMessages
+    }
+
+    static removeMessage() {
+        Comunication.arrayOfMessages.push(
+            {
+                valueOfMessage,
+                toAgent
+            }
+        )
+    }
+
+    static setArrayC
+}
+
+Comunication.start()
